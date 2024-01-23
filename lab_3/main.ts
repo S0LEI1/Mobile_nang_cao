a9ac39eb4e1e2e297a9870e1c5820e6c442c5483
S0LEI1 <vanngocpentakill@gmail.com>
2024-01-23 08:33:36 +0700

    add reference book

--- lab_3/main.ts
+++ lab_3/main.ts
@@ -1,5 +1,19 @@
 import { TextBook } from "./textbook";
+import { ReferenceBook } from './referenceBook';
 
 const date = new Date();
-const textboox = new TextBook('1',date,12,1,'ngoc', true);
-console.log(textboox.totalPrice());
+const textbooxnew = new TextBook('1',date,12,1,'ngoc', true);
+const textbooxold = new TextBook('2',date,12,1,'ngoc', false);
+const referenceBook = new ReferenceBook('3',date,12,1,'ngoc', 12);
+console.log(textbooxnew);
+console.log(textbooxnew.totalPrice());
+console.log('//////////////////////');
+
+console.log(textbooxold);
+console.log(textbooxold.totalPrice());
+console.log('//////////////////////');
+
+console.log(referenceBook);
+console.log(referenceBook.totalPrice());
+
+
